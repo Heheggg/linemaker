727d67e764e71bf8e4daf19ae0e52c70088c8a19
Sam Xu <yxu8@stuy.edu>
2017-02-12 22:23:57 -0500

    complete

--- draw.py
+++ draw.py
@@ -1,4 +1,23 @@
 from display import *
 
-def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+def draw_line(x0, y0, x1, y1, screen, color):
+    if(x0 > x1):
+        draw_line(x1, y1, x0, y0, screen, color)
+
+    else:
+        A = y1 - y0
+        B = - (x1 - x0)
+        distance = 2 * A + B
+
+        x = x0
+        y = y0
+        while x < x1:
+            plot(screen, color, x, y)
+            if distance > 0:
+                y += 1
+                distance += 2 * B
+            x += 1
+            distance += 2 * A
+
+        return screen
+        
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from random import randint
+
 from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+green = [0, 255, 0]
+blue = [0, 0, 255]
+red = [255, 0, 0]
+color = [red, green, blue]
+
+draw_line(randint(0, 100), 100, randint(200, 300), 200, screen, color[randint(0, 2)])
+draw_line(randint(0, 100), 100, randint(200, 300), 120, screen, color[randint(0, 3)])
+draw_line(randint(200, 300), 105, randint(0, 100), 100, screen, color[randint(0, 3)])
 
 display(screen)
 save_extension(screen, 'img.png')
