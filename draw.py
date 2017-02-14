21f95f93091cbb3e7efbdba9f32d2bb71d47f388
Sam Xu <yxu8@stuy.edu>
2017-02-14 15:20:45 -0500

    Cool stuff boyZ

--- draw.py
+++ draw.py
@@ -21,3 +21,44 @@ def draw_line(x0, y0, x1, y1, screen, color):
 
         return screen
         
+
+def draw_quad2():
+
+
+def draw_quadVII():
+
+
+def draw_quadVIII():
+#II
+#<x+0.5, y-1>
+#d = A + 2B
+#while y < y1
+#   plot(x,y);
+#   if d < 0
+#     x++
+#     d+= 2A
+#   y++
+#   d += 2b
+#
+
+#VII
+#<x+0.5, y-1)
+#d = A - 2B
+#while (y > y1)
+#   plot(x,y)
+#   if d > 0
+#     x++
+#     d += 2a
+#   y--
+#   d -= 2B
+
+#VIII
+#<x+1,y-0.5>
+#d = 2A - B 
+#while (x < x1)
+#   plot(x,y)
+#   if(d < 0)
+#     y--
+#     d-=2B
+#   x++
+#   d+= 2A 
