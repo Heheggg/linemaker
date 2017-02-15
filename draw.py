1d668d7be9f658c0eb9223e33575fddeee9c0048
Sam Xu <yxu8@stuy.edu>
2017-02-15 15:12:08 -0500

    alpha completes

--- draw.py
+++ draw.py
@@ -9,6 +9,17 @@ def draw_line(x0, y0, x1, y1, screen, color):
     slope = (y1 - y0)/(x1 - x0)
         
     else:
+        if slope > 1:
+            return draw_quad2(x0, y0, x1, y1, screen, color)
+        elif slope > 0:
+            return draw_quad1(x0, y0, x1, y1, screen, color)
+        elif slope > -1:
+            return draw_quad8(x0, y0, x1, y1, screen, color)
+        else:
+            return draw_quad7(x0, y0, x1, y1, screen, color)
+
+    
+def draw_quad1(x0, y0, x1, y1, screen, color)
         A = y1 - y0
         B = - (x1 - x0)
         distance = 2 * A + B
@@ -25,14 +36,61 @@ def draw_line(x0, y0, x1, y1, screen, color):
 
         return screen
         
+    
+def draw_quad2(x0, y0, x1, y1, screen, color):
+        A = y1 - y0
+        B = - (x1 - x0)
+        d = A + 2* B
+    
+        x = x0
+        y = y0
+        while y < y1
+            plot(x,y);
+            if d < 0
+                x += 1
+                d+= 2 * A
+            y += 1
+            d += 2 * b
 
-def draw_quad2():
+        return screen
+        
 
+def draw_quad7(x0, y0, x1, y1, screen, color):
+        A = y1 - y0
+        B = - (x1 - x0)
 
-def draw_quadVII():
+        x = x0
+        y = y0
+        d = A - 2 * B
+        while (y > y1)
+            plot(x,y)
+            if d > 0
+                x += 1
+                d += 2 * a
+            y -= 1
+            d -= 2 * B
 
+        return screen
+        
 
-def draw_quadVIII():
+def draw_quad8(x0, y0, x1, y1, screen, color):
+        A = y1 - y0
+        B = - (x1 - x0)
+
+        x = x0
+        y = y0
+
+        d = 2 * A - B 
+        while (x < x1)
+            plot(x,y)
+            if(d < 0)
+                y -= 1
+                d -= 2 * B
+            x +=
+            d += 2 * A
+
+        return screen
+
 #II
 #<x+0.5, y-1>
 #d = A + 2B
